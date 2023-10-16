be7c0b3ee11639315c4a44d79c0aa8288ea7d0b2
Karan <karanreddy3267@gmail.com>
2023-10-17 02:48:05 +0530

    ..

--- backend/routes/playlist.js
+++ backend/routes/playlist.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const playlist = require("../models/Playlist");
-const Song =require("../models/song")
+const Song =require("../models/Song")
 const mongoose= require("mongoose");
 const { route } = require("./auth");
 
