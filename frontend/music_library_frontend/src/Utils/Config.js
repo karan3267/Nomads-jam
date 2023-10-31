30ab6e76b7765e9edb5e6e2092a8683b7b3d91f5
Karan <karanreddy3267@gmail.com>
2023-10-31 11:42:04 +0530

    Changed backend url from local host

--- frontend/music_library_frontend/src/Utils/Config.js
+++ frontend/music_library_frontend/src/Utils/Config.js
@@ -1,3 +1,3 @@
-// export const backendUrl="https://nomads-jam-backend.onrender.com"
-export const backendUrl="http://localhost:8000"
+export const backendUrl="https://nomads-jam-backend.onrender.com"
+// export const backendUrl="http://localhost:8000"
 export const uploadPreset="alulhgz8";
